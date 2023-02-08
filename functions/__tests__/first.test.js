cbda062e2b838d3768c89b00c6110f0b14869f50
Matheus Nugas <matheusnugas@Matheuss-MacBook-Air.local>
2023-02-08 14:50:45 -0300

    Testing GA

---
+++ functions/__tests__/first.test.js
@@ -0,0 +1,56 @@
+const {
+  assertFails,
+  assertSucceeds,
+  initializeTestEnvironment,
+} = require("@firebase/rules-unit-testing");
+
+const {
+  addDoc,
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  deleteDoc,
+  getDoc,
+} = require("firebase/firestore");
+
+const projectId = "nugas-testing-projects";
+
+describe("tests firebase", () => {
+  beforeEach(async () => {
+    const testEnv = await initializeTestEnvironment({
+      projectId: projectId,
+      firestore: {
+        host: "127.0.0.1",
+        port: "8080",
+      },
+    });
+    await testEnv.clearFirestore();
+  });
+
+  afterEach(async () => {
+    const testEnv = await initializeTestEnvironment({
+      projectId: projectId,
+      firestore: {
+        host: "127.0.0.1",
+        port: "8080",
+      },
+    });
+    await testEnv.clearFirestore();
+  });
+
+  test("Test one", async () => {
+    const testEnv = await initializeTestEnvironment({
+      projectId: projectId,
+      firestore: {
+        host: "127.0.0.1",
+        port: "8080",
+      },
+    });
+    const alice = testEnv.unauthenticatedContext();
+    const firestore = alice.firestore();
+    await assertSucceeds(
+      firestore.collection("cocozinho").add({ sexo: "cocozinho2" })
+    );
+  });
+});
