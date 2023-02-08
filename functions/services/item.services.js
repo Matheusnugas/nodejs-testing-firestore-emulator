247ec62c8c8653eff2ec28a5f9831b7f1a922114
Matheus Nugas <matheusnugas@Matheuss-MacBook-Air.local>
2023-02-08 15:01:18 -0300

    Testing GA

--- functions/services/item.services.js
+++ functions/services/item.services.js
@@ -1,46 +1,45 @@
 const { addDoc, collection, getDocs, updateDoc, doc, deleteDoc, getDoc } = require('firebase/firestore')
 import db from '../models/firestore.model'
 
-export default class ItemServices  {
+export default class ItemServices {
+  async getAllItems() {
+    let resultArray = [];
+    const testingRef = collection(db, "testingDatabase");
+    const docSnap = await getDocs(testingRef);
+    docSnap.forEach((doc) => {
+      let newObject = {
+        docId: doc.id,
+        ...doc.data(),
+      };
+      resultArray.push(newObject);
+    });
+    return resultArray;
+  }
 
-      async getAllItems() {
-         let resultArray = [];
-         const testingRef = collection(db, "testingDatabase");
-         const docSnap = await getDocs(testingRef); 
-         docSnap.forEach((doc) => {
-          let newObject = {
-            docId: doc.id,
-            ...doc.data(),
-          }
-           resultArray.push(newObject)
-         });
-       return resultArray;
-      }
+  // testing
 
-    async getItemById(id) {
-      const testingRef = doc(db, "testingDatabase", id);
-      const docSnap = await getDoc(testingRef);
-      return {
-        questionId: docSnap.id,
-        ...docSnap.data()
-      }
-    }
-      
-    async createNewItem (payload) {
-        const newItem = await addDoc(collection(db, "testingDatabase"), {
-          ...payload
-        });
-        return newItem;
-    }
-   
-   async updateItemById (id, payload) {
-      const docRef = doc(db, "testingDatabase", id);
-      await updateDoc(docRef, 
-         {...payload},
-       );
-   }
+  async getItemById(id) {
+    const testingRef = doc(db, "testingDatabase", id);
+    const docSnap = await getDoc(testingRef);
+    return {
+      questionId: docSnap.id,
+      ...docSnap.data(),
+    };
+  }
 
-   async deleteItemById(id) {
-      await deleteDoc(doc(db, "testingDatabase", id));
-   }
+  async createNewItem(payload) {
+    const newItem = await addDoc(collection(db, "testingDatabase"), {
+      ...payload,
+    });
+    return newItem;
+  }
+
+  async updateItemById(id, payload) {
+    const docRef = doc(db, "testingDatabase", id);
+    await updateDoc(docRef, { ...payload });
+  }
+
+  async deleteItemById(id) {
+    await deleteDoc(doc(db, "testingDatabase", id));
+  }
 }
